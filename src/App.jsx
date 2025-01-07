9cd0da19cf8de6c90020d6099f9cdf8fea7ce9ce
Olga.Kulaga <olga.kulaga@gmail.com>
2025-01-07 18:20:43 +0100

    new2

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@
 // импорт  css
 import "./App.css";
 import Homework01 from "./homeworks/hw01/Homework01";
+//import Lesson02 from "./lessons/lesson02/lesson02";
 //import Lesson01 from "./lessons/lesson01/Lesson01";
 
 function App() {
   return (
     <>
       {/* <Lesson01/> */}
+      {/* <Lesson02/> */}
       <Homework01 />
     </>
   );
